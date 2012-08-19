16e4aee6cdd2cd5299a90627ddbfd81b78dd64a3
zachleat <zachleatherman@gmail.com>
2012-08-19 12:05:04 -0500

    Less INYOURFACE logos, only animates on hover. Removing svg picturefill since it doesn't make any sense to use it–just use <object> embed with free png fallback.

--- js/title.js
+++ js/title.js
@@ -5,20 +5,8 @@
 		return;
 	}
 
-	function cloneImages() {
-		var titleImage = doc.querySelector( '.nejs-title object, .nejs-title img' ),
-			numberOfImages = 6;
-
-		if( titleImage ) {
-			for( var j=1, k=numberOfImages; j<k; j++ ) {
-				titleImage.parentNode.insertBefore( titleImage.cloneNode( false ), titleImage );
-			}
-		}
-	}
-
 	if( win.addEventListener ){
 		win.addEventListener( "DOMContentLoaded", function(){
-			cloneImages();
 			// Run once only
 			win.removeEventListener( "load", cloneImages, false );
 		}, false );
