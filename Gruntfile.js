0385415efe842cf2f27c37aaf2061702fdd4f292
Zach Leatherman <zachleatherman@gmail.com>
2014-04-08 21:11:40 -0500

    Separate presenters onto their own page. Add code of conduct.

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,7 +132,7 @@ module.exports = function(grunt) {
 				tasks: ['default']
 			},
 			content: {
-				files: ['<%= config.root %>_posts/**/*', '<%= config.root %>_layouts/**/*', '<%= config.root %>speaking/**/*', '<%= config.root %>projects/**/*', '<%= config.root %>about/**/*', '<%= config.root %>license/**/*', '<%= config.root %>feed/**/*', '<%= config.root %>index.html', '<%= config.root %>_plugins/**/*', '<%= config.root %>_includes/**/*' ],
+				files: ['<%= config.root %>_posts/**/*', '<%= config.root %>_layouts/**/*', '<%= config.root %>speaking/**/*', '<%= config.root %>projects/**/*', '<%= config.root %>about/**/*', '<%= config.root %>license/**/*', '<%= config.root %>conduct/**/*', '<%= config.root %>presenters/**/*', '<%= config.root %>feed/**/*', '<%= config.root %>index.html', '<%= config.root %>_plugins/**/*', '<%= config.root %>_includes/**/*' ],
 				tasks: ['content']
 			},
 			config: {
